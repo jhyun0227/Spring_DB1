e3ad6924fc8b3792e0b594e134fefcec83fb73c8
준호 <83191821+jhyun0227@users.noreply.github.com>
2022-11-06 16:19:05 +0900

    # 커넥션풀과 데이터소스
    ## 커넥션 풀
    
    - 서버에서 데이터베이스 커넥션을 획득할 때에는 복잡한 과정을 거친다.
        - 고객이 애플리케이션을 사용할 때 SQL 실행하는 시간 뿐만 아니라 커넥션을 새로 만드는 시간도 추가되기 때문에 응답 속도에 영향을 준다.
    - 그래서 커넥션을 미리 만들어 놓고 사용하는 커넥션 풀 개념이 생김
    - 커넥션 풀에서 커넥션을 꺼내 사용하고 다 사용하면 다시 반환한다.
        - 커넥션을 종료하는 것이 아니라 살아있는 상태로 커넥션 풀에 반환
        - 커넥션 풀 오픈 소스는 대부분 HikariCP를 사용
    
    ## 데이터소스
    
    - 커넥션을 얻는 방법은 JDBC DriverManager를 직접 사용하거나, 커넥션풀을 사용하는 등 다양한 방법 존재
        - 커넥션 → 커넥션 풀, 커넥션 풀 → 커넥션 풀 변경 시 애플리케이션 코드도 함께 변경해야함…
    - 이런 문제를 해결하기 위해 커넥션을 획득하는 방법을 추상화하는 인터페이스
        - DI + OCP 의 원칙을 잘 사용!
        - 핵심 기능은 커넥션 조회 하나… DataSource.getConnection()
        - 대부분의 커넥션 풀은 DataSource 인터페이스를 구현해두었음
        - DriverManager도 DataSource 구현체 존재: dataSourceDriverManager

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,62 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+        Connection con2 = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+//        Connection con3 = dataSource.getConnection();
+//        Connection con4 = dataSource.getConnection();
+//        Connection con5 = dataSource.getConnection();
+//        Connection con6 = dataSource.getConnection();
+//        Connection con7 = dataSource.getConnection();
+//        Connection con8 = dataSource.getConnection();
+//        Connection con9 = dataSource.getConnection();
+//        Connection con10 = dataSource.getConnection();
+//        Connection con11 = dataSource.getConnection();
+
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+}
